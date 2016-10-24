fe69c64783a5bd7d182eda98639bf75d11fc347d
molingyu <z1422716486@hotmail.com>
2016-10-24 17:24:53 +0800

    fix a bugs
    fix bug of time transform(12 to 24)

--- lib/qmessage.rb
+++ lib/qmessage.rb
@@ -10,28 +10,40 @@ module QMsg
     end
   end
 
-  def self.run(str)
-    @messages = []
-    qq_message = nil
-    m_str = ''
-    str.split("\n").each do |m|
-      if m == "\r"
-        qq_message.message = m_str
-        @messages.push(qq_message)
-        m_str = ''
+  class << self
+    def run(str)
+      @messages = []
+      qq_message = nil
+      m_str = ''
+      str.split("\n").each do |m|
+        if m == "\r"
+          qq_message.message = m_str
+          @messages.push(qq_message)
+          m_str = ''
+        end
+        if m[/([0-9]{3}\-[0-9]{,2}\-[0-9]{,2}) ([0-9]{,2}):([0-9]{,2}):([0-9]{,2}) ([A-Z][A-Z]) (.*)\(([0-9]*)\)/]
+          data = Time.gm(*$1.split('-'), time12for24($2.to_i, $5), $3, $4)
+          u_name = $6
+          u_code = $7
+          qq_message = QQMessage.new(data, u_name, u_code, '')
+          next
+        end
+        m_str = m_str + m
       end
-      if m[/([0-9]{3}\-[0-9]{,2}\-[0-9]{,2}) ([0-9]{,2}):([0-9]{,2}):([0-9]{,2}) ([A-Z][A-Z]) (.*)\(([0-9]*)\)/]
-        data = Time.gm(*$1.split('-'), ($5 == 'PM' ? $2.to_i + 12 : $2), $3, $4)
-        u_name = $6
-        u_code = $7
-        qq_message = QQMessage.new(data, u_name, u_code, '')
-        next
-      end
-      m_str = m_str + m
+      return @messages if @messages.last == qq_message
+      qq_message.message = m_str
+      @messages.push(qq_message)
     end
-    return @messages if @messages.last == qq_message
-    qq_message.message = m_str
-    @messages.push(qq_message)
+
+    private
+    def time12for24(time, type)
+      if type == 'PM' || type == 'pm'
+        return 0 if time == 12
+        time + 12
+      else
+        time
+      end
+    end
   end
 
 end
